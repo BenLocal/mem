1e435dad0d31ba251416c631ceddb5b02a316781
benshi <807629978@qq.com>
2026-04-30 08:01:11 +0000

    feat(transcripts): add ConversationMessage / BlockType / MessageRole domain types

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,6 +1,9 @@
+pub mod conversation_message;
 pub mod embeddings;
 pub mod episode;
 pub mod memory;
 pub mod query;
 pub mod session;
 pub mod workflow;
+
+pub use conversation_message::{BlockType, ConversationMessage, MessageRole};
